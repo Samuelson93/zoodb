3685400f5c80176c7396aa6193bfe152e977e114
Samuel Ramírez Afonso <samuel_ra93@hotmail.com>
2017-06-12 20:13:42 +0100

    Proyecto del Zoo con clases creadas

---
+++ Database/ZooApp/ZooApp/Models/Clasificacion.cs
@@ -0,0 +1,8 @@
+﻿namespace ZooApp
+{
+    public class Clasificacion
+    {
+        public int id { get; set; }
+        public string denominacion { get; set; }
+    }
+}
---
+++ Database/ZooApp/ZooApp/Models/Especies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZooApp
+{
+    public class Especies
+    {
+        public long idEspecie { get; set; }
+        public string nombre { get; set; }
+        public short nPlazas { get; set; }
+        public bool esMascota { get; set; }
+        public Clasificacion Clasificacion { get; set; }
+        public  TipoAnimal TipoAnimal { get; set; }
+
+    }
+
+
+}
